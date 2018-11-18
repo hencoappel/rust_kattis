b4dd141bcc75356d4576bb549f4962e6e134f6b8
Henco Appel <hencoappel@gmail.com>
2018-11-18 12:03:19 +0000

    Finish qaly problem
    Finish up the qaly problem and change it to read from stdin as required
    by Kattis.

--- qaly/src/main.rs
+++ qaly/src/main.rs
@@ -1,27 +1,22 @@
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
+use std::io;
+use std::io::{BufRead, BufReader, Read};
 
-fn read<R: Read>(io: R) -> Result<Vec<(i64, i64)>, Error> {
-    let br = BufReader::new(io);
-    br.lines()
+fn read<R: Read>(io: R) -> Vec<(f64, f64)> {
+    let reader = BufReader::new(io);
+    return reader.lines()
         .skip(1)
         .map(|line| {
-            let split: Vec<i64> = line.unwrap()
+            let split: Vec<f64> = line.unwrap()
                 .split(" ")
                 .map(|val| val.parse().unwrap())
-                .collect()
-                .unwrap();
-            (&split[0], &split[1]);
+                .collect();
+            return (split[0], split[1]);
         })
         .collect();
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let vec = read(File::open(filename).unwrap());
-    println!("{}", vec);
+    let vec = read(io::stdin());
+    println!("{:#?}", vec.iter().map(|&(v1, v2)| v1 * v2).sum::<f64>());
 }
 
